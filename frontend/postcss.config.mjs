43a011da1a215d9f2f9cbf01fd3f674fb7cf4592
adityakamble007 <adityakamble685@gmail.com>
2025-08-06 23:20:19 +0530

    Changest in post css

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -2,16 +2,7 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
   
 };
-// tailwind.config.js (only if you want to extend default values)
-module.exports = {
-  theme: {
-    extend: {
-      backdropBlur: {
-        xs: '2px',
-      },
-    },
-  },
-};
+
 
 
 export default config;
